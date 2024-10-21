c298b69c2f3dd701f42c82b3d24a6f233d46850e
wanaxel <wanaxel@proton.me>
2024-10-21 09:20:30 +0700

    Added media paused feature and fix bug when running the code

--- visual.sh
+++ visual.sh
@@ -6,41 +6,53 @@ rows=28
 while true; do
   clear
 
-  artist=$(playerctl metadata xesam:artist)
-  title=$(playerctl metadata xesam:title)
-  position=$(playerctl position)
-  length=$(playerctl metadata mpris:length)
+  status=$(playerctl status)
 
-  length_sec=$((length / 1000000))
-  position_sec=$(echo "$position" | bc)
-  progress_percent=$(echo "scale=2; 100 * $position_sec / $length_sec" | bc)
+  if [ "$status" == "Paused" ]; then
+    paused_message="Media is Paused"
+    paused_pad=$(( (cols - ${#paused_message}) / 2 ))
+    top_padding=$(( (rows - 3) / 2 ))
 
-  bar_size=40
-  filled_bar=$(echo "$progress_percent * $bar_size / 100" | bc)
-  empty_bar=$(echo "$bar_size - $filled_bar" | bc)
-  progress_bar=$(printf '█%.0s' $(seq 1 ${filled_bar%.*}))
-  progress_bar+=$(printf '░%.0s' $(seq 1 ${empty_bar%.*}))
+    printf "%${top_padding}s\n" ""
+    printf "%${paused_pad}s%s\n\n" "" "$paused_message"
+  else
+    artist=$(playerctl metadata xesam:artist)
+    title=$(playerctl metadata xesam:title)
+    position=$(playerctl position)
+    length=$(playerctl metadata mpris:length)
 
-  position_time=$(printf "%02d:%02d" $((position_sec / 60)) $((position_sec % 60)))
-  length_time=$(printf "%02d:%02d" $((length_sec / 60)) $((length_sec % 60)))
+    length_sec=$((length / 1000000))
+    position_sec=$(printf "%.0f" "$position")  
+    progress_percent=$(echo "scale=2; 100 * $position_sec / $length_sec" | bc)
 
-  artist_line=" $artist "
-  title_line=" $title "
+    bar_size=40
+    filled_bar=$(printf "%.0f" "$(echo "$progress_percent * $bar_size / 100" | bc)")  
+    empty_bar=$(( bar_size - filled_bar ))
+    progress_bar=$(printf '█%.0s' $(seq 1 $filled_bar))
+    progress_bar+=$(printf '░%.0s' $(seq 1 $empty_bar))
 
-  artist_bigger=$(printf "%s\n\n\n" "$artist_line")
-  title_bigger=$(printf "%s\n\n" "$title_line")
+    position_time=$(printf "%02d:%02d" $((position_sec / 60)) $((position_sec % 60)))
+    length_time=$(printf "%02d:%02d" $((length_sec / 60)) $((length_sec % 60)))
 
-  artist_pad=$(( (cols - ${#artist_line}) / 2 ))
-  title_pad=$(( (cols - ${#title_line}) / 2 ))
-  progress_line="[$progress_bar] $position_time / $length_time"
-  progress_pad=$(( (cols - ${#progress_line}) / 2 ))
+    artist_line=" $artist "
+    title_line=" $title "
 
-  top_padding=$(( (rows - 10) / 2 ))
+    artist_bigger=$(printf "%s\n\n\n" "$artist_line")
+    title_bigger=$(printf "%s\n\n" "$title_line")
 
-  printf "%${top_padding}s\n" ""
-  printf "%${artist_pad}s%s\n\n" "" "$artist_bigger"
-  printf "%${title_pad}s%s\n\n" "" "$title_bigger"
-  printf "%${progress_pad}s%s\n" "" "$progress_line"
+    artist_pad=$(( (cols - ${#artist_line}) / 2 ))
+    title_pad=$(( (cols - ${#title_line}) / 2 ))
+    progress_line="[$progress_bar] $position_time / $length_time"
+    progress_pad=$(( (cols - ${#progress_line}) / 2 ))
+
+    top_padding=$(( (rows - 10) / 2 ))
+
+    printf "%${top_padding}s\n" ""
+    printf "%${artist_pad}s%s\n\n" "" "$artist_bigger"
+    printf "%${title_pad}s%s\n\n" "" "$title_bigger"
+    printf "%${progress_pad}s%s\n" "" "$progress_line"
+  fi
 
   sleep 1
 done
+
