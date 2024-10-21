f551dab30198b19771536e8c79aeaf31b44ed91e
wanaxel <wanaxel@proton.me>
2024-10-21 10:23:59 +0700

    Update remove flickering and match terminal color

--- visual.sh
+++ visual.sh
@@ -3,9 +3,15 @@
 cols=144
 rows=28
 
-while true; do
-  clear
+last_status=""
+last_artist=""
+last_title=""
+last_position=0
 
+text_color=$(tput setaf 7)
+reset_color=$(tput sgr0)
+
+while true; do
   status=$(playerctl status)
 
   if [ "$status" == "Paused" ]; then
@@ -13,8 +19,9 @@ while true; do
     paused_pad=$(( (cols - ${#paused_message}) / 2 ))
     top_padding=$(( (rows - 3) / 2 ))
 
+    printf "\033[H\033[J"
     printf "%${top_padding}s\n" ""
-    printf "%${paused_pad}s%s\n\n" "" "$paused_message"
+    printf "%${paused_pad}s%s%s\n\n" "" "$text_color$paused_message" "$reset_color"
   else
     artist=$(playerctl metadata xesam:artist)
     title=$(playerctl metadata xesam:title)
@@ -22,11 +29,11 @@ while true; do
     length=$(playerctl metadata mpris:length)
 
     length_sec=$((length / 1000000))
-    position_sec=$(printf "%.0f" "$position")  
-    progress_percent=$(echo "scale=2; 100 * $position_sec / $length_sec" | bc)
+    position_sec=$(printf "%.0f" "$position")
 
+    progress_percent=$(echo "scale=2; 100 * $position_sec / $length_sec" | bc)
     bar_size=40
-    filled_bar=$(printf "%.0f" "$(echo "$progress_percent * $bar_size / 100" | bc)")  
+    filled_bar=$(printf "%.0f" "$(echo "$progress_percent * $bar_size / 100" | bc)")
     empty_bar=$(( bar_size - filled_bar ))
     progress_bar=$(printf '█%.0s' $(seq 1 $filled_bar))
     progress_bar+=$(printf '░%.0s' $(seq 1 $empty_bar))
@@ -37,20 +44,33 @@ while true; do
     artist_line=" $artist "
     title_line=" $title "
 
-    artist_bigger=$(printf "%s\n\n\n" "$artist_line")
-    title_bigger=$(printf "%s\n\n" "$title_line")
+    if [ "$last_artist" != "$artist" ] || [ "$last_title" != "$title" ] || [ "$last_status" != "$status" ] || [ "$last_position" -ne "$position_sec" ]; then
+      printf "\033[H\033[J"
+      top_padding=$(( (rows - 8) / 2 ))
 
-    artist_pad=$(( (cols - ${#artist_line}) / 2 ))
-    title_pad=$(( (cols - ${#title_line}) / 2 ))
-    progress_line="[$progress_bar] $position_time / $length_time"
-    progress_pad=$(( (cols - ${#progress_line}) / 2 ))
+      printf "%${top_padding}s\n" ""
 
-    top_padding=$(( (rows - 10) / 2 ))
+      artist_pad=$(( (cols - ${#artist_line}) / 2 ))
+      title_pad=$(( (cols - ${#title_line}) / 2 ))
 
-    printf "%${top_padding}s\n" ""
-    printf "%${artist_pad}s%s\n\n" "" "$artist_bigger"
-    printf "%${title_pad}s%s\n\n" "" "$title_bigger"
-    printf "%${progress_pad}s%s\n" "" "$progress_line"
+      printf "%${artist_pad}s%s%s\n" "" "$text_color$artist_line" "$reset_color"
+      printf "\n"
+      printf "%${title_pad}s%s%s\n" "" "$text_color$title_line" "$reset_color"
+      printf "\n"
+
+      progress_line="[$progress_bar] $position_time / $length_time"
+      progress_pad=$(( (cols - ${#progress_line}) / 2 ))
+      printf "%${progress_pad}s%s%s\n" "" "$text_color$progress_line" "$reset_color"
+
+      last_artist="$artist"
+      last_title="$title"
+      last_status="$status"
+      last_position="$position_sec"
+    else
+      printf "\033[$((rows - 4))H"
+      printf "\033[K"
+      printf "\033[${progress_pad}C%s[%s] %s / %s%s\n" "$text_color" "$progress_bar" "$position_time" "$length_time" "$reset_color"
+    fi
   fi
 
   sleep 1
